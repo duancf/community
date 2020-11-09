dee0398ebc9ea516f4977f7ff4f9f7e3dbc21f87
duancf <duancfmail@163.com>
2020-11-09 22:30:19 +0800

    提交

---
+++ README.md
@@ -0,0 +1,5 @@
+##社区文档
+
+## 资料
+[bootstrap文档]https://v3.bootcss.com/components/?#navbar
+[Github 登录]https://docs.github.com/en/free-pro-team@latest/developers/apps/creating-an-oauth-app
---
+++ src/main/java/com/duan/community/controller/AuthorizeController.java
@@ -0,0 +1,38 @@
+package com.duan.community.controller;
+
+import com.duan.community.dto.AccessTokenDTO;
+import com.duan.community.dto.GithubUser;
+import com.duan.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String clientUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new  AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(clientUri);
+        String accessToken =  githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        //System.out.println(user.getName());
+        return "index";
+    }
+}
